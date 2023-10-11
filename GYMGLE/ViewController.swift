91f5167481fd5bca02d629f65cd7c507113ccbeb
Kong <rhd9228@gmail.com>
2023-10-11 21:11:46 +0900

    Initial Commit

---
+++ GYMGLE/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GYMGLE
+//
+//  Created by t2023-m0078 on 2023/10/11.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+        print("테스트 - s")
+    }
+
+
+}
+
