17c46c0ff0d680ec4e8420527f56110c177b763e
Kong <rhd9228@gmail.com>
2023-10-12 14:50:22 +0900

    이니셜 커밋 : 코어데이터 삭제, CryptoKit 암호화 테스트, Firebase database 테스트

--- GYMGLE/ViewController.swift
+++ GYMGLE/ViewController.swift
@@ -6,17 +6,55 @@
 //
 
 import UIKit
-import Firebase
+import FirebaseDatabase
+import CryptoKit
 
 class ViewController: UIViewController {
+    
+    private let database = Database.database().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        print("테스트 - 뷰디드로드")
-        print("a")
+        
+        //MARK: - 암호화 (CryptoKit 이용)
+        let str = "hello World"
+        let data = Data(str.utf8)
+        let digest = SHA256.hash(data: data)
+        let hash = digest.compactMap { String(format: "%02x", $0)}.joined()
+        
+        let key = SymmetricKey(size: .bits256)
+        let message = str.data(using: .utf8)!
+        let sealed = try! AES.GCM.seal(message, using: key)
+        
+        let decryptedData = try? AES.GCM.open(sealed, using: key)
+        print(String(data: decryptedData ?? Data(), encoding: .utf8) ?? "")
+        
+        //MARK: - FireBase 테스트
+        fireBaseTest(title: "제목", name: "공성표", string: "아무내용") // 등록 테스트 코드
+        
+        fireBaseReadTest("제목") // read 테스트 코드
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) { // 그냥 3초 후에 배경색 빨강으로 바꾸는 코드
+            self.view.backgroundColor = .red
+        }
     }
+    
+    private func fireBaseTest(title:String,name:String,string:String){
+        let object: [String:Any] = ["이름":name as NSObject, "그냥 스트링":string]
+        database.child(title).setValue(object)
+        
+    }
+    
+    private func fireBaseReadTest(_ title:String){
+        database.child(title).observeSingleEvent(of: .value, with: { snapshot in
+            guard let value = snapshot.value as? [String:Any] else { return }
+            print("테스트 - \(value)")
+        })
 
-
+    }
+    
+    
 }
 
