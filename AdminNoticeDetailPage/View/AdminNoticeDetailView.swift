ff7fb645c4be796886716ad41d420235a1d07fd9
seongwon <tjddnjs549@gmail.com>
2023-10-14 16:31:01 +0900

    [FEAT] : ⚙️ seongwon / AdminNotice(+Detail) 뷰, 뷰컨트롤러 파일 생성

---
+++ AdminNoticeDetailPage/View/AdminNoticeDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AdminNoticeDetailView.swift
+//  GYMGLE
+//
+//  Created by 박성원 on 2023/10/14.
+//
+
+import UIKit
+
+class AdminNoticeDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AdminNoticeDetailPage/ViewController/AdminNoticeDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AdminNoticeDetailViewController.swift
+//  GYMGLE
+//
+//  Created by 박성원 on 2023/10/14.
+//
+
+import UIKit
+
+class AdminNoticeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
