85bc583e48c3060406d6f5e4e0f47f2eb4ce1953
seongwon <tjddnjs549@gmail.com>
2023-10-14 17:28:04 +0900

    [FEAT] : ⚙️ seongwon / 공지사항 디테일뷰 구현

--- AdminNoticeDetailPage/View/AdminNoticeDetailView.swift
+++ AdminNoticeDetailPage/View/AdminNoticeDetailView.swift
@@ -11,11 +11,46 @@ final class AdminNoticeDetailView: UIView {
 
     // MARK: - UI Properties
     
+    private lazy var pageTitleLabel: UILabel = {
+        let label = UILabel()
+        label.labelMakeUI(textColor: ColorGuide.black, font: FontGuide.size32Bold, textAligment: .center)
+        label.text = "내용"
+        return label
+    }()
     
+    private let topDivider: UIView = {
+        let divider = UIView()
+        divider.backgroundColor = ColorGuide.textHint.withAlphaComponent(0.4)
+        divider.translatesAutoresizingMaskIntoConstraints = false
+        return divider
+    }()
     
+    lazy var contentTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "텍스트를 여기에 입력하세요."
+        textView.font = FontGuide.size16
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    private let bottomDivider: UIView = {
+        let divider = UIView()
+        divider.backgroundColor = ColorGuide.textHint.withAlphaComponent(0.4)
+        divider.translatesAutoresizingMaskIntoConstraints = false
+        return divider
+    }()
+    
+    lazy var createButton: UIButton = {
+        let button = UIButton()
+        button.buttonMakeUI(backgroundColor: ColorGuide.main, cornerRadius: 10.0, borderWidth: 0.0, borderColor: UIColor
+            .clear.cgColor, setTitle: "등록하기", font: FontGuide.size19Bold, setTitleColor: UIColor.white)
+        return button
+    }()
     // MARK: - init
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.backgroundColor = UIColor.white
+        viewMakeUI()
     }
     
     required init?(coder: NSCoder) {
@@ -23,3 +58,37 @@ final class AdminNoticeDetailView: UIView {
     }
 }
 // MARK: - extension
+
+private extension AdminNoticeDetailView {
+    
+    func viewMakeUI() {
+        var views = [pageTitleLabel, topDivider, contentTextView, bottomDivider, createButton]
+        for view in views {
+            self.addSubview(view)
+        }
+        NSLayoutConstraint.activate([
+            pageTitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 22),
+            pageTitleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 88),
+            
+            topDivider.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 22),
+            topDivider.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -22),
+            topDivider.heightAnchor.constraint(equalToConstant: 2),
+            topDivider.topAnchor.constraint(equalTo: self.pageTitleLabel.bottomAnchor, constant: 60),
+            
+            contentTextView.topAnchor.constraint(equalTo: self.topDivider.bottomAnchor, constant: 10),
+            contentTextView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 22),
+            contentTextView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -22),
+            contentTextView.bottomAnchor.constraint(equalTo: self.bottomDivider.topAnchor, constant: -10),
+            
+            bottomDivider.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 22),
+            bottomDivider.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -22),
+            bottomDivider.bottomAnchor.constraint(equalTo: self.createButton.topAnchor, constant: -104),
+            bottomDivider.heightAnchor.constraint(equalToConstant: 2),
+            
+            createButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 22),
+            createButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -22),
+            createButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -124),
+            createButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+}
--- AdminNoticeDetailPage/ViewController/AdminNoticeDetailViewController.swift
+++ AdminNoticeDetailPage/ViewController/AdminNoticeDetailViewController.swift
@@ -12,8 +12,6 @@ final class AdminNoticeDetailViewController: UIViewController {
     
     private let adminNoticeDetailView = AdminNoticeDetailView()
     
-    
-    
     // MARK: - life cycle
 
     override func loadView() {
@@ -22,12 +20,34 @@ final class AdminNoticeDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        viewSetting()
 
     }
-    
-
-    
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        adminNoticeDetailView.endEditing(true)
+    }
 }
 // MARK: - extension
+
+private extension AdminNoticeDetailViewController {
+    func viewSetting() {
+        adminNoticeDetailView.contentTextView.delegate = self
+    }
+}
+
+extension AdminNoticeDetailViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == "텍스트를 여기에 입력하세요." {
+            textView.text = nil
+            textView.textColor = ColorGuide.black
+        }
+    }
+    // 입력이 끝났을때
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            textView.text = "텍스트를 여기에 입력하세요."
+            textView.textColor = .lightGray
+        }
+    }
+}
