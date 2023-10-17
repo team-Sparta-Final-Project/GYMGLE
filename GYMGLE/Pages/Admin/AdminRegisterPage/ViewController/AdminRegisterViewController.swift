d8535f83e14deba8b968c9d55ab90eb1e01ffe40
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2023-10-17 12:30:38 +0900

    [FEAT] : ⚙️ 더미데이터 이용해서 InitialPage, AdminLoginPage, AdminRegisterPage 유효성 검사 추가

--- GYMGLE/Pages/Admin/AdminRegisterPage/ViewController/AdminRegisterViewController.swift
+++ GYMGLE/Pages/Admin/AdminRegisterPage/ViewController/AdminRegisterViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class AdminRegisterViewController: UIViewController {
     
+    // MARK: - Properties
     private let adminRegisterView = AdminRegisterView()
+    private var isIdDuplicated: Bool = false
+    private var isNumberDuplicated: Bool = false
+    let dataTest = DataTest.shared
 
+    // MARK: - Life Cycle
+    
     override func loadView() {
         view = adminRegisterView
     }
@@ -44,15 +50,93 @@ private extension AdminRegisterViewController {
     }
     
     @objc func validCheckButtonTapped() {
+        validCheck()
+        let alert = UIAlertController(title: "사업자 등록 번호 중복확인",
+                                      message: "중복되지 않는 사업자 등록 번호입니다.",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
         
+        isNumberDuplicated = false
     }
     
     @objc func duplicationCheckButtonTapped() {
+        idDuplicationCheck()
+        let alert = UIAlertController(title: "아이디 중복확인",
+                                      message: "사용할 수 있는 아이디입니다.",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
         
+        isIdDuplicated = false
     }
     
     @objc func registerButtonTapped() {
-        let vc = AdminRootViewController()
-        navigationController?.pushViewController(vc, animated: true)
+        if !isIdDuplicated && !isNumberDuplicated {
+            registerGym()
+            let vc = AdminRootViewController()
+            navigationController?.pushViewController(vc, animated: true)
+        } else {
+            let alert = UIAlertController(title: "등록할 수 없습니다.",
+                                          message: "올바르지 않은 정보가 있습니다. 다시 입력해주세요.",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func idDuplicationCheck() {
+        for gymInfo in dataTest.gymList {
+            if gymInfo.gymAccount.id == adminRegisterView.idTextField.text {
+                let alert = UIAlertController(title: "중복된 아이디",
+                                              message: "중복된 아이디입니다. 다른 아이디를 입력해주세요.",
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+                
+                isIdDuplicated = true
+                
+                return
+            }
+        }
+    }
+    
+    func validCheck() {
+        for gymInfo in dataTest.gymList {
+            if gymInfo.gymnumber == adminRegisterView.registerNumberTextField.text {
+                let alert = UIAlertController(title: "사업자 등록 번호 중복",
+                                              message: "사업자 등록 번호 중복입니다. 다시 입력해주세요.",
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+                
+                isNumberDuplicated = true
+                
+                return
+            }
+        }
+    }
+    
+    func registerGym() {
+        if let id = adminRegisterView.idTextField.text,
+           let password = adminRegisterView.passwordTextField.text,
+           let gymName = adminRegisterView.adminNameTextField.text,
+           let gymPhoneNumber = adminRegisterView.adminNameTextField.text,
+           let gymNumber = adminRegisterView.registerNumberTextField.text {
+            let account = Account(id: id, password: password, accountType: 3)
+            let gymInfo = GymInfo(gymAccount: account, gymName: gymName, gymPhoneNumber: gymPhoneNumber, gymnumber: gymNumber, gymUserList: [], noticeList: [], gymInAndOutLog: [])
+            
+            if id.count >= 1 && password.count >= 1 && gymName.count >= 1 && gymPhoneNumber.count >= 1 && gymNumber.count == 10 {
+                dataTest.gymList.append(gymInfo)
+                print(dataTest.gymList)
+            } else {
+                let alert = UIAlertController(title: "등록할 수 없습니다.",
+                                              message: "올바르지 않은 정보가 있습니다. 다시 입력해주세요.",
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+                return
+            }
+        }
     }
 }
