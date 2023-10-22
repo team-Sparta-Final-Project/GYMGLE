f8d8f0b18729e0e37ea39b8aab385ea48d64116b
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2023-10-22 18:09:09 +0900

    [DESIGN] : 🎨 MyPage UI 수정,  [FEAT] : ⚙️ 공지사항, 로그아웃, 탈퇴하기 추가

---
+++ GYMGLE/Helpers/Protocol_Delegate.swift
@@ -0,0 +1,12 @@
+//
+//  Protocol_Delegate.swift
+//  GYMGLE
+//
+//  Created by 조규연 on 10/22/23.
+//
+
+import Foundation
+
+protocol MyPageTableViewDelegate: AnyObject {
+    func didSelectCell(at indexPath: IndexPath)
+}
