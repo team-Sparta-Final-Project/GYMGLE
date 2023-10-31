d57fd3e22117f76420aa73fb407ec6b7c0db0bae
seongwon <tjddnjs549@gmail.com>
2023-10-31 21:05:39 +0900

    [FEAT] : ⚙️ seongwon /  커뮤니티 모델 생성

---
+++ GYMGLE/Model/Board.swift
@@ -0,0 +1,31 @@
+//
+//  Board.swift
+//  GYMGLE
+//
+//  Created by 박성원 on 10/31/23.
+//
+
+import Foundation
+
+struct Board: Codable {
+    let uid: String
+    var content: String
+    var date: Date
+    var isUpdated: Bool
+    var likeCount: Int
+}
+
+//프로필
+struct Profile: Codable {
+    var nickName: String
+    var image: String
+}
+
+//코멘트
+struct Comment: Codable {
+    let uid: String
+    var comment: String
+    var date: Date
+    var isUpdated: Bool
+}
+
