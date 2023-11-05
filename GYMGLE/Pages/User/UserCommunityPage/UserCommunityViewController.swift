35d7b072e15e2a25d40b79b16ad591590b44769a
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2023-11-05 18:25:02 +0900

    [Feat] 차단기능 적용

--- GYMGLE/Pages/User/UserCommunityPage/UserCommunityViewController.swift
+++ GYMGLE/Pages/User/UserCommunityPage/UserCommunityViewController.swift
@@ -23,7 +23,9 @@ class UserCommunityViewController: UIViewController, CommunityTableViewDelegate 
     
     let first = UserCommunityView()
     let second = CommunityCell()
-
+    
+    var blockedUsers: [String] = [] // 차단한 유저
+    
     override func loadView() {
         view = first
         
@@ -38,23 +40,67 @@ class UserCommunityViewController: UIViewController, CommunityTableViewDelegate 
     override func viewWillAppear(_ animated: Bool) { // 네비게이션바 보여주기
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = false
-        decodeData {
+        //        decodeData {
+        //            self.first.appTableView.reloadData()
+        //        }
+        fetchPosts {
             self.first.appTableView.reloadData()
         }
     }
     
     @objc func writePlaceTap() {
         let userCommunityWriteViewController = UserCommunityWriteViewController()
-//        navigationController?.pushViewController(userCommunityWriteViewController, animated: true)
+        //        navigationController?.pushViewController(userCommunityWriteViewController, animated: true)
         userCommunityWriteViewController.modalPresentationStyle = .fullScreen
         self.present(userCommunityWriteViewController, animated: true)
+    }
+    
+    func fetchPosts(completion: @escaping () -> Void) {
+        let ref = Database.database().reference().child("accounts/\(Auth.auth().currentUser!.uid)/blockedUserList")
+        ref.observeSingleEvent(of: .value) { (snapshot) in
+            if snapshot.exists() {
+                if let blockedUserData = snapshot.value as? [String: Bool] {
+                    // 차단 유저 리스트 배열로 저장
+                    self.blockedUsers = Array(blockedUserData.keys)
+                    
+                    let databaseRef = Database.database().reference().child("boards")
+                    let numberOfPostsToRetrieve = 30  // 가져올 게시물 개수 (원하는 개수로 수정)
+                    
+                    databaseRef.queryOrdered(byChild: "date")
+                        .queryLimited(toLast: UInt(numberOfPostsToRetrieve))
+                        .observeSingleEvent(of: .value) { snapshot in
+                            self.first.posts.removeAll() // 데이터를 새로 받을 때 배열 비우기
+                            for childSnapshot in snapshot.children {
+                                if let snapshot = childSnapshot as? DataSnapshot,
+                                   let data = snapshot.value as? [String: Any],
+                                   let key = snapshot.key as? String,
+                                   let uid = data["uid"] as? String {
+                                    
+                                    // 차단한 유저인지 확인
+                                    if !self.blockedUsers.contains(uid) {
+                                        do {
+                                            let dataInfoJSON = try JSONSerialization.data(withJSONObject: data, options: [])
+                                            let dataInfo = try JSONDecoder().decode(Board.self, from: dataInfoJSON)
+                                            self.first.posts.insert(dataInfo, at: 0) 
+                                            self.first.keys.append(key)
+                                        } catch {
+                                            print("디코딩 에러")
+                                        }
+                                    }
+                                }
+                            }
+                            completion()
+                        }
+                }
+            }
+        }
     }
 }
 
 extension UserCommunityViewController {
     func decodeData(completion: @escaping () -> Void) {
         let databaseRef = Database.database().reference().child("boards")
-
+        
         let numberOfPostsToRetrieve = 30  // 가져올 게시물 개수 (원하는 개수로 수정)
         databaseRef.queryOrdered(byChild: "date")
             .queryLimited(toLast: UInt(numberOfPostsToRetrieve))
@@ -62,8 +108,8 @@ extension UserCommunityViewController {
                 self.first.posts.removeAll() // 데이터를 새로 받을 때 배열 비우기
                 for childSnapshot in snapshot.children {
                     if let snapshot = childSnapshot as? DataSnapshot,
-                        let data = snapshot.value as? [String: Any],
-                        let key = snapshot.key as? String {
+                       let data = snapshot.value as? [String: Any],
+                       let key = snapshot.key as? String {
                         do {
                             let dataInfoJSON = try JSONSerialization.data(withJSONObject: data, options: [])
                             let dataInfo = try JSONDecoder().decode(Board.self, from: dataInfoJSON)
@@ -76,7 +122,7 @@ extension UserCommunityViewController {
                 }
                 completion()
                 // 테이블 뷰에 업데이트된 순서대로 표시
-//                self.appTableView.reloadData()
+                //                self.appTableView.reloadData()
             }
     }
 }
