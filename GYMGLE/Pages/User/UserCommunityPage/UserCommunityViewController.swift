df3933c174f799e872333f90b1fca2e2e7525792
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2023-11-06 16:21:12 +0900

    [Fix] 차단기능 버그 해결

--- GYMGLE/Pages/User/UserCommunityPage/UserCommunityViewController.swift
+++ GYMGLE/Pages/User/UserCommunityPage/UserCommunityViewController.swift
@@ -41,10 +41,12 @@ class UserCommunityViewController: UIViewController, CommunityTableViewDelegate 
     override func viewWillAppear(_ animated: Bool) { // 네비게이션바 보여주기
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = false
-
-        fetchPosts {
-            self.downloadProfiles {
-                self.first.appTableView.reloadData()
+        
+        getBlockedUserList {
+            self.decodeData {
+                self.downloadProfiles {
+                    self.first.appTableView.reloadData()
+                }
             }
         }
         observeFirebaseDataChanges()
@@ -57,46 +59,16 @@ class UserCommunityViewController: UIViewController, CommunityTableViewDelegate 
         self.present(userCommunityWriteViewController, animated: true)
     }
     
-    func fetchPosts(completion: @escaping () -> ()) {
+    func getBlockedUserList(completion: @escaping () -> ()) {
         let ref = Database.database().reference().child("accounts/\(Auth.auth().currentUser!.uid)/blockedUserList")
         ref.observeSingleEvent(of: .value) { (snapshot) in
             if snapshot.exists() {
                 if let blockedUserData = snapshot.value as? [String: Bool] {
                     // 차단 유저 리스트 배열로 저장
                     self.blockedUsers = Array(blockedUserData.keys)
-                    
-                    let databaseRef = Database.database().reference().child("boards")
-                    let numberOfPostsToRetrieve = 30  // 가져올 게시물 개수 (원하는 개수로 수정)
-                    
-                    databaseRef.queryOrdered(byChild: "date")
-                        .queryLimited(toLast: UInt(numberOfPostsToRetrieve))
-                        .observeSingleEvent(of: .value) { snapshot in
-                            self.first.posts.removeAll() // 데이터를 새로 받을 때 배열 비우기
-                            self.first.keys.removeAll()
-                            for childSnapshot in snapshot.children {
-                                if let snapshot = childSnapshot as? DataSnapshot,
-                                   let data = snapshot.value as? [String: Any],
-                                   let key = snapshot.key as? String,
-                                   let uid = data["uid"] as? String {
-                                    
-                                    // 차단한 유저인지 확인
-                                    if !self.blockedUsers.contains(uid) {
-                                        do {
-                                            let dataInfoJSON = try JSONSerialization.data(withJSONObject: data, options: [])
-                                            let dataInfo = try JSONDecoder().decode(Board.self, from: dataInfoJSON)
-                                            self.first.posts.insert(dataInfo, at: 0) 
-                                            self.first.keys.append(key)
-                                        } catch {
-                                            print("디코딩 에러2")
-                                        }
-                                    }
-                                }
-                            }
-                            completion()
-                        }
-//                    completion()
                 }
             }
+            completion()
         }
     }
 }
@@ -110,18 +82,23 @@ extension UserCommunityViewController {
             .queryLimited(toLast: UInt(numberOfPostsToRetrieve))
             .observeSingleEvent(of: .value) { snapshot in
                 self.first.posts.removeAll() // 데이터를 새로 받을 때 배열 비우기
+                self.first.keys.removeAll()
                 for childSnapshot in snapshot.children {
                     if let snapshot = childSnapshot as? DataSnapshot,
                        let data = snapshot.value as? [String: Any],
-                       let key = snapshot.key as? String {
-                        do {
-                            let dataInfoJSON = try JSONSerialization.data(withJSONObject: data, options: [])
-                            let dataInfo = try JSONDecoder().decode(Board.self, from: dataInfoJSON)
-                            self.first.posts.insert(dataInfo, at: 0) // 가장 최근 게시물을 맨 위에 추가
-                            self.first.keys.insert(key, at: 0)
-                            print("테스트 - \(data["comments"])")
-                        } catch {
-                            print("디코딩 에러")
+                       let key = snapshot.key as? String,
+                       let uid = data["uid"] as? String {
+                        
+                        // 차단한 유저인지 확인
+                        if !self.blockedUsers.contains(uid) {
+                            do {
+                                let dataInfoJSON = try JSONSerialization.data(withJSONObject: data, options: [])
+                                let dataInfo = try JSONDecoder().decode(Board.self, from: dataInfoJSON)
+                                self.first.posts.insert(dataInfo, at: 0)
+                                self.first.keys.insert(key, at: 0)
+                            } catch {
+                                print("디코딩 에러2")
+                            }
                         }
                     }
                 }
@@ -144,7 +121,7 @@ extension UserCommunityViewController {
                 }
             }
         }
-
+        
         let ref = Database.database().reference()
         var temp:[String] = []
         var tempProfiles:[String:Profile] = [:]
@@ -163,18 +140,20 @@ extension UserCommunityViewController {
             }
             
         }
-
+        
     }
     func observeFirebaseDataChanges() {
         let databaseRef = Database.database().reference().child("boards")
-
+        
         databaseRef.observe(.value) { snapshot in
-            self.decodeData {
-                self.downloadProfiles {
-                    self.first.appTableView.reloadData()
+            self.getBlockedUserList {
+                self.decodeData {
+                    self.downloadProfiles {
+                        self.first.appTableView.reloadData()
+                    }
                 }
             }
         }
     }
-
+    
 }
