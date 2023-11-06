695af91718aebb6d97a756a8ac61eeb65895519f
seongwon <tjddnjs549@gmail.com>
2023-11-06 10:51:17 +0900

    [DESIGN] : 🎨 seongwon / 프로필 디자인 수정

--- GYMGLE/Pages/User/UserMyProfilePage/View/UserMyProfileView.swift
+++ GYMGLE/Pages/User/UserMyProfilePage/View/UserMyProfileView.swift
@@ -23,8 +23,6 @@ final class UserMyProfileView: UIView {
         //image.image = UIImage(systemName: "goforward")?.withRenderingMode(.alwaysOriginal)
         image.contentMode = .scaleAspectFill
         image.clipsToBounds = true
-        image.layer.borderWidth = 1.2
-        image.layer.borderColor = ColorGuide.white.cgColor
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
@@ -48,7 +46,7 @@ final class UserMyProfileView: UIView {
     }()
     
     private lazy var labelStackView: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [gymName, nickName, postCountLabel])
+        let stack = UIStackView(arrangedSubviews: [gymName, nickName])
         stack.spacing = 2
         stack.axis = .vertical
         stack.distribution = .fill
@@ -68,7 +66,7 @@ final class UserMyProfileView: UIView {
     
     lazy var updateButton: UIButton = {
         let button = UIButton()
-        button.buttonImageMakeUI(backgroundColor: ColorGuide.white, image: "pencil", color: ColorGuide.black, cornerRadius: 14, borderWidth: 0.0, borderColor: UIColor.white.cgColor)
+        button.buttonMakeUI(backgroundColor: ColorGuide.profileButton, cornerRadius: 8, borderWidth: 0.0, borderColor: UIColor.clear.cgColor, setTitle: "프로필 수정", font: FontGuide.size19, setTitleColor: ColorGuide.profileButtonLabel)
         return button
     }()
     
@@ -105,17 +103,11 @@ private extension UserMyProfileView {
     }
     
     func viewSetting() {
-        self.addSubviews(updateButton)
         self.addSubviews(banButton)
         NSLayoutConstraint.activate([
-            updateButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-            updateButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 80),
-            updateButton.widthAnchor.constraint(equalToConstant: 28),
-            updateButton.heightAnchor.constraint(equalToConstant: 28),
-            
             banButton.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 90),
             banButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-            banButton.topAnchor.constraint(equalTo: updateButton.bottomAnchor, constant: 40),
+            banButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 148),
             banButton.bottomAnchor.constraint(equalTo: containerView.topAnchor, constant: -6)
         ])
     }
@@ -124,6 +116,8 @@ private extension UserMyProfileView {
         self.addSubviews(containerView)
         containerView.addSubview(profileImageView)
         containerView.addSubview(labelStackView)
+        containerView.addSubview(updateButton)
+        containerView.addSubview(postCountLabel)
         containerView.addSubview(postTableview)
         
         NSLayoutConstraint.activate([
@@ -143,9 +137,17 @@ private extension UserMyProfileView {
             labelStackView.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 4),
             labelStackView.widthAnchor.constraint(equalToConstant: 240),
             
+            updateButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            updateButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            updateButton.topAnchor.constraint(equalTo: self.labelStackView.bottomAnchor, constant: 10),
+           
+            postCountLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            postCountLabel.topAnchor.constraint(equalTo: self.updateButton.bottomAnchor, constant: 40),
+            postCountLabel.bottomAnchor.constraint(equalTo: self.postTableview.topAnchor, constant: -12),
+            
             postTableview.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
             postTableview.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
-            postTableview.topAnchor.constraint(equalTo: labelStackView.bottomAnchor, constant: 6),
+            postTableview.topAnchor.constraint(equalTo: postCountLabel.bottomAnchor, constant: 6),
             postTableview.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 0)
         ])
     }
