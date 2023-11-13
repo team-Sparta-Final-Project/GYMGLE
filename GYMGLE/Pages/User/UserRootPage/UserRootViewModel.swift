7ff6ab9b53390ac14989374626ea2cb211e00993
Oong2 <ehddns0528@gmail.com>
2023-11-13 10:01:26 +0900

    User Root view model 추가중

--- GYMGLE/Pages/User/UserRootPage/UserRootViewModel.swift
+++ GYMGLE/Pages/User/UserRootPage/UserRootViewModel.swift
@@ -1,8 +1,38 @@
-//
-//  UserRootViewModel.swift
-//  GYMGLE
-//
-//  Created by t2023-m0088 on 11/13/23.
-//
+// UserRootViewModel.swift
 
-import Foundation
+import Combine
+
+class UserRootViewModel {
+
+    var userNameSubject = PassthroughSubject<String, Never>()
+    var gymInUserCountSubject = PassthroughSubject<String, Never>()
+    var yesterUserCountSubject = PassthroughSubject<String, Never>()
+    var noticeSubject = PassthroughSubject<String, Never>()
+
+    var userName: String = "" {
+        didSet {
+            userNameSubject.send(userName)
+        }
+    }
+
+    var gymInUserCount: String = "" {
+        didSet {
+            gymInUserCountSubject.send(gymInUserCount)
+        }
+    }
+
+    var yesterUserCount: String = "" {
+        didSet {
+            yesterUserCountSubject.send(yesterUserCount)
+        }
+    }
+
+    var notice: String = "" {
+        didSet {
+            noticeSubject.send(notice)
+        }
+    }
+
+    init() {
+    }
+}
