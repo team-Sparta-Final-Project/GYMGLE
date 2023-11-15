65da4e770028d24de1bd4cf9db5781bde52e20a9
seongwon <tjddnjs549@gmail.com>
2023-11-15 15:00:47 +0900

    [REFACTOR] : ♻️ seongwon /  공지사항 부분 콤바인 적용

--- GYMGLE/Pages/Admin/AdminNoticePage/AdminNoticeDetailPage/ViewController/AdminNoticeDetailViewController.swift
+++ GYMGLE/Pages/Admin/AdminNoticePage/AdminNoticeDetailPage/ViewController/AdminNoticeDetailViewController.swift
@@ -3,19 +3,18 @@
 //  GYMGLE
 //
 //  Created by 박성원 on 2023/10/14.
-// 3060349969 -> 사업자 등록 번호 (임시 사용)
+//
+
 import UIKit
-import FirebaseAuth
-import FirebaseCore
-import FirebaseDatabase
+import Combine
 
 final class AdminNoticeDetailViewController: UIViewController {
     
     // MARK: - properties
     private let adminNoticeDetailView = AdminNoticeDetailView()
-    var viewModel: AdminNoticeDetailViewModel!
-    var noticeInfo: Notice?
-    var isUser: Bool?
+    var viewModel: AdminNoticeDetailViewModel = AdminNoticeDetailViewModel()
+    var disposableBag = Set<AnyCancellable>()
+    
     // MARK: - life cycle
     override func loadView() {
         view = adminNoticeDetailView
@@ -23,7 +22,6 @@ final class AdminNoticeDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viewConfigure()
-        viewModel = AdminNoticeDetailViewModel()
         if adminNoticeDetailView.contentTextView.text == "500자 이내로 공지사항을 적어주세요!" {
             adminNoticeDetailView.deletedButton.isHidden = true
         }
@@ -46,30 +44,37 @@ private extension AdminNoticeDetailViewController {
     }
 
     func viewSetting() {
-        if let noticeInfo = self.noticeInfo {
-            adminNoticeDetailView.contentTextView.text = noticeInfo.content
-            adminNoticeDetailView.contentNumberLabel.text = "\(adminNoticeDetailView.contentTextView.text.count)/500"
-            adminNoticeDetailView.createButton.setTitle("수정하기", for: .normal)
-            adminNoticeDetailView.deletedButton.isHidden = false
-        } else {
-            adminNoticeDetailView.createButton.setTitle("등록하기", for: .normal)
-        }
+        viewModel.$noticeInfo.sink { [self] in
+            if let noticeInfo = $0 {
+                adminNoticeDetailView.contentTextView.text = noticeInfo.content
+                adminNoticeDetailView.contentNumberLabel.text = "\(adminNoticeDetailView.contentTextView.text.count)/500"
+                adminNoticeDetailView.createButton.setTitle("수정하기", for: .normal)
+                adminNoticeDetailView.deletedButton.isHidden = false
+            } else {
+                adminNoticeDetailView.createButton.setTitle("등록하기", for: .normal)
+                adminNoticeDetailView.contentNumberLabel.text = "0/500"
+            }
+        }.store(in: &disposableBag)
     }
     func buttonTapped() {
         adminNoticeDetailView.createButton.addTarget(self, action: #selector(createButtonTapped), for: .touchUpInside)
         adminNoticeDetailView.deletedButton.addTarget(self, action:  #selector(deletedButtonTapped), for: .touchUpInside)
-        switch isUser {
-        case false: //유저가 들어오면
-            adminNoticeDetailView.createButton.isHidden = true
-            adminNoticeDetailView.deletedButton.isHidden = true
-            adminNoticeDetailView.contentTextView.isEditable = false
-            adminNoticeDetailView.contentNumberLabel.isHidden = true
-        default:
-            adminNoticeDetailView.createButton.isHidden = false
-            adminNoticeDetailView.deletedButton.isHidden = false
-            adminNoticeDetailView.contentTextView.isEditable = true
-            adminNoticeDetailView.contentNumberLabel.isHidden = false
-        }
+        viewModel.$isUser.sink { [weak self] isUser in
+            guard let self = self else { return }
+            switch isUser {
+            case false: //유저가 들어오면
+                adminNoticeDetailView.createButton.isHidden = true
+                adminNoticeDetailView.deletedButton.isHidden = true
+                adminNoticeDetailView.contentTextView.isEditable = false
+                adminNoticeDetailView.contentNumberLabel.isHidden = true
+            default:
+                adminNoticeDetailView.createButton.isHidden = false
+                adminNoticeDetailView.deletedButton.isHidden = false
+                adminNoticeDetailView.contentTextView.isEditable = true
+                adminNoticeDetailView.contentNumberLabel.isHidden = false
+            }
+        }.store(in: &disposableBag)
+       
     }
 }
 // MARK: - @objc func
@@ -79,22 +84,24 @@ extension AdminNoticeDetailViewController {
         if adminNoticeDetailView.contentTextView.text.isEmpty || adminNoticeDetailView.contentTextView.text == "500자 이내로 공지사항을 적어주세요!" {
             showToast(message: "내용물이 비었습니다.", view: self.view,  bottomAnchor: -80, widthAnchor: 220, heightAnchor: 30)
         } else {
-            if noticeInfo == nil {
-                let date = Date()
-                let content = adminNoticeDetailView.contentTextView.text
-                let newNotice = Notice(date: date, content: content ?? "")
-                viewModel.createdNoticeData(notice: newNotice) {
-                    self.navigationController?.popViewController(animated: true)
-                }
-            } else {
-                if var notice = noticeInfo {
-                    let oldContent = notice.content
-                    notice.content = adminNoticeDetailView.contentTextView.text
-                    viewModel.updatedNoticeData(oldContent: oldContent, newNotice: notice) {
+            viewModel.$noticeInfo.sink { [self] in
+                if $0 == nil {
+                    let date = Date()
+                    let content = adminNoticeDetailView.contentTextView.text
+                    let newNotice = Notice(date: date, content: content ?? "")
+                    viewModel.createdNoticeData(notice: newNotice) {
                         self.navigationController?.popViewController(animated: true)
                     }
+                } else {
+                    if var notice = $0 {
+                        let oldContent = notice.content
+                        notice.content = adminNoticeDetailView.contentTextView.text
+                        viewModel.updatedNoticeData(oldContent: oldContent, newNotice: notice) {
+                            self.navigationController?.popViewController(animated: true)
+                        }
+                    }
                 }
-            }
+            }.store(in: &disposableBag)
         }
     }
     
@@ -103,11 +110,15 @@ extension AdminNoticeDetailViewController {
                                       message: "삭제하시겠습니까?",
                                       preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { _ in
-            if let notice = self.noticeInfo {
-                self.viewModel.deletedNotice(notice: notice, completion: {
-                    self.navigationController?.popViewController(animated: true)
-                })
-            }
+            self.viewModel.$noticeInfo.sink { [weak self] in
+                guard let self = self else {return}
+                if let notice = $0 {
+                    viewModel.deletedNotice(notice: notice, completion: {
+                        self.navigationController?.popViewController(animated: true)
+                        DataManager.shared.noticeList.removeAll(where: {$0.date == notice.date})
+                    })
+                }
+            }.store(in: &self.disposableBag)
         }))
         alert.addAction(UIAlertAction(title: "취소", style: .cancel))
         present(alert, animated: true, completion: nil)
